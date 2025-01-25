9846a6c9d3f6b00912ea7cbd37651f5f50d688c6
sivasanjeevs <sivasanjeevs0@gmail.com>
2025-01-25 20:44:18 +0530

    design

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -8,36 +8,42 @@ const HomePage = () => {
 
 	return (
 		<>
-			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen bg-black'>
+			<div className="flex-[4_4_0] mx-auto border-r border-gray-700 min-h-screen bg-black max-w-2xl">
 				{/* Header */}
-				<div className='flex w-full border-b border-gray-700'>
+				<div className="flex w-full border-b border-gray-700">
 					<div
-						className={
-							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-						}
+						className={`flex justify-center flex-1 p-3 transition duration-300 cursor-pointer relative ${
+							feedType === "forYou" ? "bg-gray-800" : "hover:bg-gray-900"
+						}`}
 						onClick={() => setFeedType("forYou")}
 					>
-						<span className='text-white'>For you</span>
+						<span className="text-white font-medium">For you</span>
 						{feedType === "forYou" && (
-							<div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>
+							<div className="absolute bottom-0 w-10 h-1 rounded-full bg-blue-500"></div>
 						)}
 					</div>
 					<div
-						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
+						className={`flex justify-center flex-1 p-3 transition duration-300 cursor-pointer relative ${
+							feedType === "following" ? "bg-gray-800" : "hover:bg-gray-900"
+						}`}
 						onClick={() => setFeedType("following")}
 					>
-						<span className='text-white'>Following</span>
+						<span className="text-white font-medium">Following</span>
 						{feedType === "following" && (
-							<div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>
+							<div className="absolute bottom-0 w-10 h-1 rounded-full bg-blue-500"></div>
 						)}
 					</div>
 				</div>
 
 				{/* CREATE POST INPUT */}
-				<CreatePost />
+				<div className="p-4 border-b border-gray-700">
+					<CreatePost />
+				</div>
 
 				{/* POSTS */}
-				<Posts feedType={feedType} />
+				<div className="p-4">
+					<Posts feedType={feedType} />
+				</div>
 			</div>
 		</>
 	);
