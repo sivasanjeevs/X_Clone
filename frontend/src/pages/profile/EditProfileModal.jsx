b5293a5207c6f24e5ddd3892f51abd469e4cb76f
sivasanjeevs <sivasanjeevs0@gmail.com>
2025-01-27 21:45:08 +0530

    buttons update

--- frontend/src/pages/profile/EditProfileModal.jsx
+++ frontend/src/pages/profile/EditProfileModal.jsx
@@ -41,20 +41,20 @@ const EditProfileModal = ({ authUser }) => {
 				Edit profile
 			</button>
 			<dialog id="edit_profile_modal" className="bg-black text-white modal">
-				<div className="modal-box border rounded-2xl border-gray-700 shadow-md bg-black">
+				<div className="modal-box border rounded-2xl border-gray-700 shadow-md bg-black p-6 space-y-6">
 					<h3 className="font-bold text-lg my-3">Update Profile</h3>
 					<form
-						className="flex flex-col gap-4"
+						className="flex flex-col gap-6"
 						onSubmit={(e) => {
 							e.preventDefault();
 							updateProfile(formData);
 						}}
 					>
-						<div className="flex flex-wrap gap-2">
+						<div className="flex flex-wrap gap-4">
 							<input
 								type="text"
 								placeholder="Full Name"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.fullName}
 								name="fullName"
 								onChange={handleInputChange}
@@ -62,34 +62,34 @@ const EditProfileModal = ({ authUser }) => {
 							<input
 								type="text"
 								placeholder="Username"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.username}
 								name="username"
 								onChange={handleInputChange}
 							/>
 						</div>
-						<div className="flex flex-wrap gap-2">
+						<div className="flex flex-wrap gap-4">
 							<input
 								type="email"
 								placeholder="Email"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.email}
 								name="email"
 								onChange={handleInputChange}
 							/>
 							<textarea
 								placeholder="Bio"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.bio}
 								name="bio"
 								onChange={handleInputChange}
 							/>
 						</div>
-						<div className="flex flex-wrap gap-2">
+						<div className="flex flex-wrap gap-4">
 							<input
 								type="password"
 								placeholder="Current Password"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.currentPassword}
 								name="currentPassword"
 								onChange={handleInputChange}
@@ -97,7 +97,7 @@ const EditProfileModal = ({ authUser }) => {
 							<input
 								type="password"
 								placeholder="New Password"
-								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+								className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 								value={formData.newPassword}
 								name="newPassword"
 								onChange={handleInputChange}
@@ -106,12 +106,15 @@ const EditProfileModal = ({ authUser }) => {
 						<input
 							type="text"
 							placeholder="Link"
-							className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-2 input-md"
+							className="flex-1 input border border-gray-700 bg-black text-white rounded-xl p-3 input-md"
 							value={formData.link}
 							name="link"
 							onChange={handleInputChange}
 						/>
-						<button className="bg-white text-black rounded-full btn-sm">
+						<button
+							className="bg-blue-600 hover:bg-blue-700 text-white rounded-full py-3 text-lg font-semibold"
+							type="submit"
+						>
 							{isUpdatingProfile ? "Updating..." : "Update"}
 						</button>
 					</form>
